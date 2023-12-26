ce8fa5816ebfe3c942c950c7d844c41a06bf0361
Mateusz <mateusz.kunik1996@gmail.com>
2023-12-26 17:03:50 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An AI-powered web application that can serve as your strength training coach, guiding you through exercises like squats, bench presses or deadlifts.',
+    author='Mateusz Kunik',
+    license='MIT',
+)
