5357890af089c9cedebdb465b71916a02a776630
Mateusz <mateusz.kunik1996@gmail.com>
2024-01-02 21:24:13 +0100

    Added several utility functions

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,11 +1,18 @@
 import numpy as np
+import pandas as pd
+import cv2
+
+from pathlib import Path
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 
+from custom_pose_landmarks import CustomPoseLandmark
 
-mp_pose = solutions.pose
 
 def landmark2array(landmark):
+    """
+    
+    """
     return np.array(
         [
             landmark.x,
@@ -15,7 +22,11 @@ def landmark2array(landmark):
         ]
     )
 
+
 def array2landmark(array):
+    """
+    
+    """
     return landmark_pb2.NormalizedLandmark(
         x=array[0],
         y=array[1],
@@ -23,26 +34,58 @@ def array2landmark(array):
         visibility=array[3]
     )
 
-def get_custom_landmarks(indexes, landmarks):
+
+def calculate_thorax_coordinates(mp_pose, landmarks):
+    """
+    Based on left and right shoulder coordinates function calculates the coordinates of thorax.
+    """
+
+    # Extract left and right shoulder landmarks and convert it to array
+    left_shoulder = landmark2array(
+        landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER])
+    right_shoulder = landmark2array(
+        landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER])
+    
+    # Calculate the thorax coordinates and convert an array to landmark
+    thorax = array2landmark(
+        np.mean([left_shoulder, right_shoulder], axis=0)
+    )
+
+    return thorax
+
+
+def get_custom_landmarks(mp_pose, custom_pose, landmarks):
     """
     
     """
     # Create customize landmarks list
     custom_landmarks = landmark_pb2.NormalizedLandmarkList()
 
-    # Extend list by chosen landmarks
+    # Extend list by selected landmarks
     custom_landmarks.landmark.extend(
-        [landmarks.landmark[index] for index in indexes])
+        [landmarks.landmark[index] for index in custom_pose.values])
 
-    # Calculate the coordinates of neck landmark
-    left_shoulder = landmark2array(
-        landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER])
-    right_shoulder = landmark2array(
-        landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER])
-    neck = np.mean([left_shoulder, right_shoulder], axis=0)
-    neck_landmark = array2landmark(neck)
+    # Calculate the coordinates of thorax landmark
+    thorax = calculate_thorax_coordinates(mp_pose, landmarks)
 
-    # Add neck landmark to custom list
-    custom_landmarks.landmark.add().CopyFrom(neck_landmark)
+    # Add thorax landmark to custom list
+    custom_landmarks.landmark.add().CopyFrom(thorax)
 
     return custom_landmarks
+
+
+def prepare_dataframe(custom_pose):
+    """
+    
+    """
+    axes = ['X', 'Y', 'Z']
+    names = custom_pose.get_landmarks().values()
+    column_names = [('_'.join([name, axis])).lower() for name in names for axis in axes]
+
+    return pd.DataFrame(
+        columns = ['Id', 'timestamp'] + column_names,
+        dtype = object
+    )
+
+
+
