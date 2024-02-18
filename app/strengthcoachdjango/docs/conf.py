f076a79160d6d64fde5a15300bfa6f37f14b8600
Krzysztof Knop <knop.krzysiek@gmail.com>
2024-02-18 16:55:45 +0100

    change in docs settings

--- app/strengthcoachdjango/docs/conf.py
+++ app/strengthcoachdjango/docs/conf.py
@@ -12,6 +12,7 @@
 
 import os
 import sys
+
 import django
 
 if os.getenv("READTHEDOCS", default=False) == "True":
@@ -19,7 +20,7 @@ if os.getenv("READTHEDOCS", default=False) == "True":
     os.environ["DJANGO_READ_DOT_ENV_FILE"] = "True"
     os.environ["USE_DOCKER"] = "no"
 else:
-    sys.path.insert(0, os.path.abspath("/app"))
+    sys.path.insert(0, os.path.abspath("../"))
 os.environ["DATABASE_URL"] = "sqlite:///readthedocs.db"
 os.environ["CELERY_BROKER_URL"] = os.getenv("REDIS_URL", "redis://redis:6379")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
@@ -40,6 +41,7 @@ author = "Krzysztof Knop"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,7 +57,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "nature"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
